99212fb79306948b4f08cad94e6bbfe05d19e049
abkhan505 <khanaban505@gmail.com>
2025-02-21 22:20:05 -0500

    Added appropriate file dependencies and fixed method calls

--- src/p2p/Peer.java
+++ src/p2p/Peer.java
@@ -3,6 +3,9 @@ package p2p;
 import java.net.*;
 import java.io.*;
 import java.util.*;
+import com.google.gson.Gson; // For Jay: Make sure this library is installed, go to File > Project Structure > Libraries > hit the + > click on Maven > search up Gson Google and install the x.10 version, standard format to use JSON's in Java
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 public class Peer {
     private DatagramSocket socket;
@@ -54,7 +57,7 @@ public class Peer {
 
         // Method to create a home directory just in case there isn't one for the node
         private void initHomeDirectory() {
-            File file = new File(homeDirectory);
+            File file = new File(pathToHomeDir);
             if (!file.exists()) {
                 try (FileWriter writer = new FileWriter(file)) {
                     writer.write("[]");
@@ -71,9 +74,9 @@ public class Peer {
                 File file = new File(pathToHomeDir);
                 if (!file.exists()) return new ArrayList<>();
 
-                BufferedReader reader = new BufferedReader(new File(file));
+                BufferedReader reader = new BufferedReader(new FileReader(file));
                 return new Gson().fromJson(reader, List.class);
-            } catch (IOException) {
+            } catch (IOException e) {
                 e.printStackTrace();
                 return new ArrayList<>();
             }
@@ -82,7 +85,7 @@ public class Peer {
         // Method to add new item to the home directory
         public void addItemToDir(String item) {
             try {
-                List<String> items = getItems();
+                List<String> items = retrieveDirItems();
                 items.add(item);
                 try (FileWriter writer = new FileWriter(pathToHomeDir)) {
                     writer.write(new Gson().toJson(items));
