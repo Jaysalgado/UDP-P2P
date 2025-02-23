5ab58dee3177ccc725191fbf5979a895c94e6a67
jaysalgado <jaysalgado97@gmail.com>
2025-02-22 19:31:38 -0500

    heartbeat start

--- src/p2p/main.java
+++
@@ -1,9 +0,0 @@
-package p2p;
-
-public class Main {
-    public static void main(String[] args) {
-        Peer peer = new Peer(9876);
-        new Thread(peer::recieve).start();
-        new Thread(() -> peer.send("Hello", "localhost")).start();
-    }
-}
